d7382fb14bb4d6afc85d606f97515d3b68b0fba2
Limo123123 <90343619+Limo123123@users.noreply.github.com>
2025-03-06 14:47:46 +0100

    Update esp8266.ts
    BETA: Added get from Teamspeak

--- esp8266.ts
+++ esp8266.ts
@@ -43,123 +43,33 @@ namespace esp8266 {
     }
 
     /**
-     * Send data to ThinkSpeak
+     * Retrieve data from ThingSpeak
      */
-    //% block="Send Data to your ThinkSpeak Channel|Write API Key %apiKey|Field1 %field1|Field2 %field2|Field3 %field3|Field4 %field4|Field5 %field5|Field6 %field6|Field7 %field7|Field8 %field8"
-    //% block.loc.de="Sende Daten an deinen ThinkSpeak Kanal|Write API Key %apiKey|Feld 1 %field1||Feld 2 %field2|Feld 3 %field3|Feld 4 %field4|Feld 5 %field5|Feld 6 %field6|Feld 7 %field7|Feld 8 %field8"
-    //% expandableArgumentMode="enabled"
-    //% apiKey.defl="your Write API Key"
-    export function sendToThinkSpeak(apiKey: string, field1: number=0, field2: number=0, field3: number=0, field4: number=0, field5: number=0, field6: number=0, field7: number=0, field8: number=0) {
+    //% block="Get Data from ThingSpeak|Read API Key %apiKey|Channel ID %channelID|Field %field"
+    export function getFromThinkSpeak(apiKey: string, channelID: string, field: number): number {
         let result = 0
-        let retry = 2
+        let data = ""
 
-        // close the previous TCP connection
         if (isWifiConnected) {
             sendAtCmd("AT+CIPCLOSE")
             waitAtResponse("OK", "ERROR", "None", 2000)
         }
 
-        while (isWifiConnected && retry > 0) {
-            retry = retry - 1;
-            // establish TCP connection
-            sendAtCmd("AT+CIPSTART=\"TCP\",\"api.thingspeak.com\",80")
-            result = waitAtResponse("OK", "ALREADY CONNECTED", "ERROR", 2000)
-            if (result == 3) continue
+        sendAtCmd(`AT+CIPSTART="TCP","api.thingspeak.com",80`)
+        result = waitAtResponse("OK", "ALREADY CONNECTED", "ERROR", 2000)
+        if (result == 3) return NaN
 
-            let data = "GET /update?api_key=" + apiKey
-            if (!isNaN(field1)) data = data + "&field1=" + field1
-            if (!isNaN(field2)) data = data + "&field2=" + field2
-            if (!isNaN(field3)) data = data + "&field3=" + field3
-            if (!isNaN(field4)) data = data + "&field4=" + field4
-            if (!isNaN(field5)) data = data + "&field5=" + field5
-            if (!isNaN(field6)) data = data + "&field6=" + field6
-            if (!isNaN(field7)) data = data + "&field7=" + field7
-            if (!isNaN(field8)) data = data + "&field8=" + field8
+        let request = `GET /channels/${channelID}/fields/${field}.json?api_key=${apiKey}&results=1`;
+        sendAtCmd(`AT+CIPSEND=${request.length + 2}`)
+        result = waitAtResponse(">", "OK", "ERROR", 2000)
+        if (result == 3) return NaN
 
-            sendAtCmd("AT+CIPSEND=" + (data.length + 2))
-            result = waitAtResponse(">", "OK", "ERROR", 2000)
-            if (result == 3) continue
-            sendAtCmd(data)
-            result = waitAtResponse("SEND OK", "SEND FAIL", "ERROR", 5000)
-
-            // // close the TCP connection
-            // sendAtCmd("AT+CIPCLOSE")
-            // waitAtResponse("OK", "ERROR", "None", 2000)
-
-            if (result == 1) break
-        }
+        sendAtCmd(request)
+        data = serial.readString()
+        let value = parseFloat(data.split("field" + field + "\":\"")[1]?.split("\"")[0] || "NaN")
+        
+        sendAtCmd("AT+CIPCLOSE")
+        waitAtResponse("OK", "ERROR", "None", 2000)
+        return value
     }
-
-    /**
-     * Send data to IFTTT
-     */
-    //% block="Send Data to your IFTTT Event|Event %event|Key %key|value1 %value1||value2 %value2|value3 %value3"
-    //% block.loc.de="Sende Daten an dein IFTTT Event|Event %event|Key %key|Wert 1 %value1|Wert 2 %value2|Wert 3 %value3"
-    //% event.defl="your Event"
-    //% key.defl="your Key"
-    //% value1.defl="Hello"
-    //% value2.defl="Calliope"
-    //% value3.defl="mini"
-    export function sendToIFTTT(event: string, key: string, value1: string, value2: string, value3: string) {
-        let result = 0
-        let retry = 2
-
-        // close the previous TCP connection
-        if (isWifiConnected) {
-            sendAtCmd("AT+CIPCLOSE")
-            waitAtResponse("OK", "ERROR", "None", 2000)
-        }
-
-        while (isWifiConnected && retry > 0) {
-            retry = retry - 1;
-            // establish TCP connection
-            sendAtCmd("AT+CIPSTART=\"TCP\",\"maker.ifttt.com\",80")
-            result = waitAtResponse("OK", "ALREADY CONNECTED", "ERROR", 2000)
-            if (result == 3) continue
-
-            let data = "GET /trigger/" + event + "/with/key/" + key
-            data = data + "?value1=" + value1
-            data = data + "&value2=" + value2
-            data = data + "&value3=" + value3
-            data = data + " HTTP/1.1"
-            data = data + "\u000D\u000A"
-            data = data + "User-Agent: curl/7.58.0"
-            data = data + "\u000D\u000A"
-            data = data + "Host: maker.ifttt.com"
-            data = data + "\u000D\u000A"
-            data = data + "Accept: */*"
-            data = data + "\u000D\u000A"
-
-            sendAtCmd("AT+CIPSEND=" + (data.length + 2))
-            result = waitAtResponse(">", "OK", "ERROR", 2000)
-            if (result == 3) continue
-            sendAtCmd(data)
-            result = waitAtResponse("SEND OK", "SEND FAIL", "ERROR", 5000)
-            // close the TCP connection
-            // sendAtCmd("AT+CIPCLOSE")
-            // waitAtResponse("OK", "ERROR", "None", 2000)
-            if (result == 1) break
-        }
-    }
-
-    function waitAtResponse(target1: string, target2: string, target3: string, timeout: number) {
-        let buffer = ""
-        let start = input.runningTime()
-
-        while ((input.runningTime() - start) < timeout) {
-            buffer += serial.readString()
-
-            if (buffer.includes(target1)) return 1
-            if (buffer.includes(target2)) return 2
-            if (buffer.includes(target3)) return 3
-
-            basic.pause(100)
-        }
-
-        return 0
-    }
-
-    function sendAtCmd(cmd: string) {
-        serial.writeString(cmd + "\u000D\u000A")
-    }
-}
+} 
